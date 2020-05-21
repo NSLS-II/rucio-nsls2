5f2e4d090c051f86c21cc16f8ff03450135cd75a
Garrett Bischof <gbischof@bnl.gov>
2020-05-21 16:47:20 -0400

    remove self from rucio_register

--- rucio_nsls2/__init__.py
+++ rucio_nsls2/__init__.py
@@ -82,7 +82,7 @@ def _get_filenames(beamline_name, run):
     return files
 
 
-def _rucio_register(self, filenames):
+def _rucio_register(filenames):
     """
     Register the file in rucio for replication to SDCC.
     """
@@ -90,16 +90,16 @@ def _rucio_register(self, filenames):
     dids = []
 
     for filename in filenames:
-        size = os.stat(str(filename)).st_size
+        #size = os.stat(str(filename)).st_size
         #adler = adler32(str(filename))
-        files.append({'scope': self.scope, 'name': str(filename.parts[-1]),
-                      'bytes': size, 'adler32': "unknown",
-                      'pfn': self.pfn + filename)})
+        files.append({'scope': scope, 'name': str(filename.parts[-1]),
+                      'bytes': 1000, 'adler32': "unknown",
+                      'pfn': pfn + filename)})
 
     replica_client = ReplicaClient()
-    replica_client.add_replicas(rse=self.rse, files=files)
+    replica_client.add_replicas(rse=rse, files=files)
     didclient = DIDClient()
-    didclient.add_files_to_dataset(self.scope, self.dataset, files)
+    didclient.add_files_to_dataset(scope, dataset, files)
 
 
 def cache_runs(catalog, run_uids, lifetime):
